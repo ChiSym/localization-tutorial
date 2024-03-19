e6a1a6231305b382fae6d4a39f416db83472860e
Jay Pottharst <sharlaon@gmail.com>
2024-03-19 18:11:17 -0400

    Revert param

--- probcomp-localization-tutorial.jl
+++ probcomp-localization-tutorial.jl
@@ -1792,7 +1792,7 @@ N_particles = 10
 ESS_threshold = 1. + N_particles / 10.
 
 grid_n_points_start = [3, 3, 3]
-grid_sizes_start = [.7, .7, π/4]
+grid_sizes_start = [.7, .7, π/10]
 grid_args_schedule = [(grid_n_points_start, grid_sizes_start .* (2/3)^(j-1)) for j=1:4]
 
 infos = particle_filter_rejuv_infos(full_model, T, full_model_args, constraints_low_deviation, N_particles, ESS_threshold, grid_smcp3_kernel, grid_args_schedule)
@@ -1812,7 +1812,7 @@ N_particles = 10
 ESS_threshold = 1. + N_particles / 10.
 
 grid_n_points_start = [3, 3, 3]
-grid_sizes_start = [.7, .7, π/4]
+grid_sizes_start = [.7, .7, π/10]
 grid_args_schedule = [(grid_n_points_start, grid_sizes_start .* (2/3)^(j-1)) for j=1:3]
 
 traces = [simulate(full_model, (T, full_model_args...)) for _ in 1:N_samples]
